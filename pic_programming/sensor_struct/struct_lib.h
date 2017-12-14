d403de5ff6bb075af152e37b28a956f32390d1ef
[sascha] <[sascha.marquardt@ua.pt]>
2017-12-14 15:41:21 +0000

    add an global structure for the sensors, change the datatype to double/int -> we should never use char for numeric values!

--- pic_programming/sensor_struct/struct_lib.h
+++ pic_programming/sensor_struct/struct_lib.h
@@ -1,44 +1,64 @@
 
+#ifndef SENSOR_STRUCT_STRUCT_LIB_H
+#define SENSOR_STRUCT_STRUCT_LIB_H
+
+/*
+ declare a sensor values structure inside of your function -> inside of that structure u can 
+ * find all structures defined
+ * 
+ * use: 
+ * 
+ *  sensorvalues sensor_values;
+ *  sensor_values.air_temperature_sensor[2].temp=20; -> setting values
+ *  int temp =sensor_values.air_temperature_sensor[2].temp -> reading values
+ * 
+ * 
+ * The structure should be defined locally as static -> access for other functions just 
+ * with getter and setter functions
+ */
+
 struct water_data
 {
-    char temp;
-    char state;
+    double temp;
+    int water_heater; /*its turned on/off*/
 };
 
-struct ligth_data
+struct light_data
 {
-    char state;
+    int movement_sensor;
+    int mechanical_switch;
 };
 
 struct air_quality_data
 {
-    char p10;
-    char co;
-    char co2;
-    char o3;
-    char humity;
-    char state;
+    double p10;
+    double co;
+    double co2;
+    double o3;
+    double humity;
+    int state;
 };
 
 struct air_temperature_data
 {
-    char temp;
-    char state;
+    double temp;
+    int state;
 };
 
 typedef struct water_data water;
-typedef struct ligth_data ligths[8];
+typedef struct light_data lights[8];
 typedef struct air_quality_data air_quality[4];
 typedef struct air_temperature_data air_temperature[8];
 
+typedef struct {
+    water water_temperature;
+    lights light_sensors;
+    air_quality air_quality_sensor;
+    air_temperature air_temperature_sensor;
+}sensorvalues;
 
-/*
- //How to use:
- air_quality air_q; // this create a variable that use the array struct
- 
- //i will do function in sensors files that you can use like this:
- get_air_quality( &air_q ); // the funct =>  void get_air_quality(air_quality_data *air_q ){...}
- */
 /* *****************************************************************************
  End of File
  */
+
+#endif //SENSOR_STRUCT_STRUCT_LIB_H
