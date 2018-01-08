fe11792285b71e9f62281d06ce7d027c76b2383e
Ricardo <jony.ricardo95@gmail.com>
2018-01-08 20:35:23 +0000

    set functions for schedules

--- final/schedules/schedules.c
+++ final/schedules/schedules.c
@@ -1,5 +1,23 @@
 #include "schedules.h"
 #include <stdio.h>
 
+int setId(int arrayFunction, int arrayRoom, int arraySchedule, int schedule, int id) {
+    home.array_function[arrayFunction].array_room[arrayRoom].array_schedule[arraySchedule].schedules[schedule].id=id;
+}
 
+int setValue(int arrayFunction, int arrayRoom, int arraySchedule, int schedule, int value) {
+    home.array_function[arrayFunction].array_room[arrayRoom].array_schedule[arraySchedule].schedules[schedule].value=value;
+}
+
+int setEnable(int arrayFunction, int arrayRoom, int arraySchedule, int schedule, int enable) {
+    home.array_function[arrayFunction].array_room[arrayRoom].array_schedule[arraySchedule].schedules[schedule].enable=enable;
+}
+
+int setStartTime(int arrayFunction, int arrayRoom, int arraySchedule, int schedule, int time) {
+    home.array_function[arrayFunction].array_room[arrayRoom].array_schedule[arraySchedule].schedules[schedule].start_time=time;
+}
+
+int setStopTime(int arrayFunction, int arrayRoom, int arraySchedule, int schedule, int time) {
+    home.array_function[arrayFunction].array_room[arrayRoom].array_schedule[arraySchedule].schedules[schedule].stop_time=time;
+}
 //static house home; /*home definition is supposed to be here!*/
