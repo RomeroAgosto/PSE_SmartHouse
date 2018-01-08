9da1a4ed0aaf61ac6c3316617f0cb7b47e95af6c
Sascha <sascha.marquardt@ua.pt>
2018-01-08 19:53:52 +0000

    space delted

--- final/Statemachines.X /build/default/production/statemachine_airtemp_control.o.d
+++
@@ -1,7 +0,0 @@
-build/default/production/statemachine_airtemp_control.o:  \
- statemachine_airtemp_control.c  \
-statemachine_airtemp_control.h  \
- statemachines.h  \
-statemachine_airquality_control.h  \
- statemachine_watertemp_control.h  \
-statemachine_light_control.h 
