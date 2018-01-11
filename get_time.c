cd01c6a85763d3c5ea5dbbe1db86e1fff6032dd9
Samuel Simoes <samuel.simoes94@gmail.com>
2018-01-11 22:14:00 +0000

    Update time on get_time

--- get_time.c
+++ get_time.c
@@ -3,9 +3,7 @@
 static time_t read_time;
 int get_time(char *to_send)
 {
-    struct tm *prt_ts;
-    time(&read_time);
-    prt_ts=gmtime(&read_time);
+    struct tm time;
 
     int i,year_value,month_value,day_value,hour_value,minute_value,second_value;
     char n_year[4], n_month[2], n_day[2],n_hour[2],n_min[2], n_sec[2],weekday_value;
@@ -24,18 +22,19 @@ int get_time(char *to_send)
     weekday_value=to_send[8];
     sscanf(n_year,"%4d",&year_value);
     sscanf(n_month,"%2d",&month_value);
-    sscanf(n_day,"%4d",&day_value);
+    sscanf(n_day,"%2d",&day_value);
     sscanf(n_hour,"%2d",&hour_value);
     sscanf(n_min,"%2d",&minute_value);
     sscanf(n_sec,"%2d",&second_value);
 
-    prt_ts->tm_year=year_value-1900;
-    prt_ts->tm_mon=month_value-1;
-    prt_ts->tm_wday=weekday_value-'0';
-    prt_ts->tm_mday=day_value;
-    prt_ts->tm_hour=hour_value;
-    prt_ts->tm_min=minute_value;
-    prt_ts->tm_sec=second_value;
+    time.tm_year=year_value-1900;
+    time.tm_mon=month_value-1;      /*! jan=1/dez=12 */
+    time.tm_wday=weekday_value-'1'; /*! Monday=1/sunday=7 */
+    time.tm_mday=day_value;
+    time.tm_hour=hour_value;
+    time.tm_min=minute_value;
+    time.tm_sec=second_value;
 
-
+    update_time(&time);
+    return 0;
 }
