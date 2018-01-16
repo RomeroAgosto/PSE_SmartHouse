55e05a6c49bd36016d18ef75d14900b81ccae248
Ricardo <jony.ricardo95@gmail.com>
2018-01-16 21:53:06 +0000

    log functions.h

--- final/log/log_functions.h
+++ final/log/log_functions.h
@@ -12,22 +12,22 @@
 #include <string.h>
 #include "../update/update.h"
 #include "../Statemachines.X/AirQuality/Statemachine_AirQuality/statemachine_airquality_control.h"
-
+ 
 /** 
 * @brief this function saves the sensor information and saves it
  * 
 */
-
+ 
 int log_data_saving();
-
-
+ 
+ 
 /** 
 * @brief This functions creates a message with all the information present in the log
 * @param  message receve the pointer to put the message
 */
-
+ 
 int log_create_msg(char *message);
-
+ 
 /** 
 * @brief This functions creates a message with all the information present in the log
 * @param  message receve the pointer to put a string
