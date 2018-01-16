d28b4fb22a9bba5217e7914f32444e8839c83e4d
Ricardo <jony.ricardo95@gmail.com>
2018-01-16 21:53:40 +0000

    log functions.h

--- final/log/log_functions.h
+++ final/log/log_functions.h
@@ -1,12 +1,10 @@
-/** \file log_functions.h
-* \brief functions that allow log management
+/** @file log_functions.h
+* @author Deep Impact
+* @date 15 Dez 2017
+* @brief functions that allow log management
 *  
 *
-* 
 *
-* \author Rafael Almeida
-*
-* \date 15-12-2017
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,29 +12,24 @@
 #include <string.h>
 #include "../update/update.h"
 #include "../Statemachines.X/AirQuality/Statemachine_AirQuality/statemachine_airquality_control.h"
-
+ 
 /** 
-* \brief this function saves the sensor information and saves it
- * \author Rafael Almeida 
-* \date 14-12-2017  
+* @brief this function saves the sensor information and saves it
+ * 
 */
-
+ 
 int log_data_saving();
-
-
+ 
+ 
 /** 
-* \brief This functions creates a message with all the information present in the log
-* \param  message receve the pointer to put a string
- * \author Rafael Almeida 
-* \date 03-11-2017  
+* @brief This functions creates a message with all the information present in the log
+* @param  message receve the pointer to put the message
 */
-
+ 
 int log_create_msg(char *message);
-
+ 
 /** 
-* \brief This functions creates a message with all the information present in the log
-* \param  message receve the pointer to put a string
- * \author Samuel Simoes 
-* \date 10-01-2018  
+* @brief This functions creates a message with all the information present in the log
+* @param  message receve the pointer to put a string
 */
 void set_log_flag();
