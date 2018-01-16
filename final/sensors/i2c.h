ed8343ee905b7034a8b4f3519f0100382cfcbad9
Ricardo <jony.ricardo95@gmail.com>
2018-01-16 23:03:03 +0000

    some .h changed

--- final/sensors/i2c.h
+++ final/sensors/i2c.h
@@ -1,10 +1,12 @@
-/** \file  i2c.h
- *  \brief File header of functions used by the I2C1 module, connected to SDA1 <br>
- *         and SCL1 pins of the PIC32MX795F512L. 
+/**
+ * @file i2c.h
+ * @author Deep Impact
+ * @date 15 Dez 2017
+ * @brief File header of functions used by the I2C1 module, connected to SDA1 and SCL1 pins of the PIC32MX795F512L. 
  * 
- *  \author Bruno Barbosa NMEC 68150
+ * Company  Deep Impact
  * 
- *  \date 18/12/2017
+ * ==============================================
  */
 
 #ifndef I2C_H
@@ -28,81 +30,66 @@
 #define ADDR_WR_TC74 ((SENS_ADDRESS_TC74 << 1) | WRITE) // Device ID + WRITE bit
 #define ADDR_RD_TC74 ((SENS_ADDRESS_TC74 << 1) | READ)  // Device ID + READ bit
 
-/** \brief Initializes the I2C module.
- *         
- * \author Bruno Barbosa NMEC 68150 
- *  
- * \date 18/12/2017
- */
+/** @brief i2c1_init, Initializes the I2C module.
+ * ==============================================
+ */ 
+
 void i2c1_init(void);
 
-/** \brief Sets up the clock signal for the SCL1 pin.
- *         
- * \author Bruno Barbosa NMEC 68150 
- * 
+/** @brief i2c1_set_clock, Sets up the clock signal for the SCL1 pin.
+ *  
  * @param clock_freq - frequency of the slave clock.
- *  
- * \date 18/12/2017
+ * @return 0
  */
-int i2c1_setClock(unsigned int clock_freq);
 
-/** \brief Starts up the I2C module.
- *         
- * \author Bruno Barbosa NMEC 68150 
- *  
- * \date 18/12/2017
+int i2c1_set_clock(unsigned int clock_freq);
+
+/** @brief i2c1_start, Starts up the I2C module.
+ *       
  * 
  */
+
 void i2c1_start(void);
 
-/** \brief Stops the I2C module.
- *         
- * \author Bruno Barbosa NMEC 68150 
- *  
- * \date 18/12/2017
+/** @brief i2c1_stop, Stops the I2C module.
+ * 
  */
+
 void i2c1_stop(void);
 
-/** \brief Sends data to state desired command (READ OR WRITE) or to select <br>
- *         slave by device ID. Returns acknowledge status bit of the slave to the <br>
- *         master device.
- *         
- * \author Bruno Barbosa NMEC 68150 
- * 
- * @param value - acknowledge bit of the slave device.
+/** @brief i2c1_send Sends data to state desired command (READ OR WRITE) or to select <br>
+ *         		slave by device ID. Returns acknowledge status bit of the slave to the <br>
+ *         		master device.
  *  
- * \date 18/12/2017
+ * @param value - acknowledge bit of the slave device.
+ * @return 0
+ *  
  */
+
 int i2c1_send(unsigned char value);
 
-/** \brief Receives data byte(s) from slave device and returns it. Sends <br>
+/** @brief i2c1_receiver, Receives data byte(s) from slave device and returns it. Sends <br>
  *         acknowledge status bit of the master to the slave.
- *         
- * \author Bruno Barbosa NMEC 68150 
  * 
  * @param ack_bit - acknowledge bit of the master device.
- *  
- * \date 18/12/2017
  */
+
 char i2c1_receive(char ack_bit);
-/** \brief Returns temperature value reading from TC74 sensor.
- *         
- * \author Bruno Barbosa NMEC 68150 
- *  
- * \date 18/12/2017
+/** @brief i2c1_s7, Returns temperature value reading from TC74 sensor.
+ *   
+ * @return 0
  */
+
 int i2c1_s7();
 
-/** \brief Humidity and temperature value reading from HIH8120 sensor.
- *         
- * \author Bruno Barbosa NMEC 68150 
- * 
+/** @brief i2c1_s8, Humidity and temperature value reading from HIH8120 sensor.
+ *   
  * @param *humi_temp[2] - pointers to register the humidity and temperature <br>
  *                        reading from the sensor. First value is the humidity <br>
  *                        value, second value is the temperature value.
- *  
- * \date 18/12/2017
+ * @return 0
  */
+
 int i2c1_s8(int *humi_temp); 
 #endif	/* I2C_H */
 
