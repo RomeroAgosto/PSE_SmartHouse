8c77ea1c0e251eed6648f9f6f8f625a0d9e5f14b
Samuel Simoes <samuel.simoes94@gmail.com>
2018-01-16 17:35:28 +0000

    Dogyxen for actuaters

--- final/actuators/actuators.h
+++ final/actuators/actuators.h
@@ -1,29 +1,43 @@
-/* 
- * File:   actuators.h
- * Author: Ricardo
- *
- * Created on 18 December 2017, 06:33
+ 
+/**
+ * @file actuators.h
+ * @author Deep Impact
+ * @date 15 Dez 2017
+ * @brief This file is use to turn ON/OFF ligths, heaters, ventilator and buzzer
+ * 
+ * This system will have:
+ * 1 water heater; 
+ * 4 ligth, that are using by the movement sensors; 
+ * 8 air heaters; 
+ * 1 ventilator; 
+ * 1 buzzer.
+ * 
+ * Company  Deep Impact
+ * 
+ * ==============================================
  */
-
+ 
 #ifndef ACTUATORS_H
-#define	ACTUATORS_H
-
+#define  ACTUATORS_H
+ 
 #include <p32xxxx.h>
 #include <stdio.h>
-
+ 
+/**@def GREEN
+* State GREEN=0
+*/
 #define GREEN 0
+/**@def YELLOW
+* State YELLOW=1
+*/
 #define YELLOW 1
+/**@def RED
+* State RED=2
+*/
 #define RED 2
-    
-    
-int set_light(int n_light, int state);
-int set_heater(int room, int state);
-int set_ventilator_buzzer(int room);
-
-
-#ifdef	__cplusplus
-}
-#endif
-
-#endif	/* ACTUATORS_H */
-
+ 
+/**
+ * @brief set_ligth,this funtion will turn ON/OFF the lights
+ * 
+commit:b8e4bf
+WIP on master
